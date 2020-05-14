312b10f3044c7e23900ad6fae26d4bdd17b77847
amanpreet9391 <amanpreet9391@gmail.com>
2020-05-14 16:53:16 +0530

    producer config changes to ensure minimum data loss(safe producer)

--- src/main/java/Twitter/TwitterProducer.java
+++ src/main/java/Twitter/TwitterProducer.java
@@ -129,7 +129,11 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION,"5");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        
         //create a kafka producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
         return producer;
