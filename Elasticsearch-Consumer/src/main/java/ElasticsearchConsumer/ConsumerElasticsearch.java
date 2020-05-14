50c71521f063640ff2fe7feff5a9a1e197afd423
amanpreet9391 <amanpreet9391@gmail.com>
2020-05-14 21:48:55 +0530

    Kafka-consumer which will store tweets in elasticsearch

--- Elasticsearch-Consumer/src/main/java/ElasticsearchConsumer/ConsumerElasticsearch.java
+++ Elasticsearch-Consumer/src/main/java/ElasticsearchConsumer/ConsumerElasticsearch.java
@@ -6,27 +6,37 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indexlifecycle.StopILMRequest;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
 
 public class ConsumerElasticsearch {
     // create an elasticsearch client
 
     public static RestHighLevelClient createClient() {
 
-
+        /** Enter your own hostname, username and password. you can use bonsai.io to get free elasticsearch cluster with 3 nodes.     */
         String hostname = "";
         String username = "";
         String password = "";
+
         // need to provide credentials in order to access elasticsearch cluster in cloud
         final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
@@ -45,17 +55,55 @@ public class ConsumerElasticsearch {
         //returning a client which will allow us to insert data in elasticsearch.
 
     }
+    public static KafkaConsumer<String, String> createConsumer(String topic){
+        String bootstrapServer = "127.0.0.1:9092";
+        String groupId = "twitter-consumer";
+        //String topic = "twitter-tweets";
+        //consumer properties
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        //create kafka consumer
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(properties);
 
-    public static void main(String[] args) throws IOException {
+        consumer.subscribe(Arrays.asList(topic));
+        return consumer;
+    }
+
+
+
+
+
+
+    public static void main(String[] args) throws IOException, InterruptedException {
         Logger logger = LoggerFactory.getLogger(ConsumerElasticsearch.class.getName());
-        String jsonString = "{ \"foo \" : \" bar \"      }";
+
+        /** just used for testing */
+        //String jsonString = "{ \"foo \" : \" bar \"      }";
         RestHighLevelClient client = createClient();
         //This index request will fail if index "twitter" doesnot exits.
-        IndexRequest indexRequest = new IndexRequest("twitter","tweets").source(jsonString, XContentType.JSON);
-        IndexResponse indexResponse =client.index(indexRequest, RequestOptions.DEFAULT);
-        String id = indexResponse.getId();
-        logger.info(id);
-        client.close();
+
+
+        KafkaConsumer<String, String> consumer = createConsumer("twitter-tweets") ;
+            while(true){
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+                for(ConsumerRecord<String,String> record: records){
+                //where we insert data into elasticsearch
+                    record.value();
+                    IndexRequest indexRequest = new IndexRequest("twitter","tweets").source(record.value(), XContentType.JSON);
+                    IndexResponse indexResponse =client.index(indexRequest, RequestOptions.DEFAULT);
+                    String id = indexResponse.getId();
+                    logger.info(id);
+                    Thread.sleep(1000);  //introduced small delay to see results clearly
+
+                }
+
+
+        }
+            ///client.close();
         //This will insert jsonString text in the index(twitter) and return id to us.
     }
 
