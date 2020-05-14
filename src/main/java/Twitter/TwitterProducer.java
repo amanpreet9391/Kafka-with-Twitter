6f3848d0bf6cfa1c99ead9fe7394776597ab0b2a
amanpreet9391 <amanpreet9391@gmail.com>
2020-05-14 19:57:37 +0530

    added high throughput properties

--- src/main/java/Twitter/TwitterProducer.java
+++ src/main/java/Twitter/TwitterProducer.java
@@ -133,7 +133,12 @@ public class TwitterProducer {
         properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION,"5");
         properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
         properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
-        
+
+        //high throughput properties
+
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG,Integer.toString(32*1024)); //32KB batch size
         //create a kafka producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
         return producer;
